ca777a4b20e17e0653bcfd061a7da279f632b520
moonelysian <dandyblack@naver.com>
2019-08-19 19:45:11 +0900

    3009

---
+++ baekjun/3009.py
@@ -0,0 +1,11 @@
+x_points=[]
+y_points=[]
+
+for _ in range(3):
+    x,y = list(map(int, input().split()))
+    x_points.append(x)
+    y_points.append(y)
+
+x = x_points[2] if x_points[0] == x_points [1] else (x_points[1] if x_points[0] == x_points[2] else x_points[0])
+y = y_points[2] if y_points[0] == y_points [1] else (y_points[1] if y_points[0] == y_points[2] else y_points[0])
+print('{} {}'.format(x, y))
