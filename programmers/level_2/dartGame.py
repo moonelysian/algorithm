71fbfb0f527c52d09073df1177f18843d954eabe
moonelysian <dandyblack@naver.com>
2020-02-19 22:30:38 +0900

    Modify [카카오코테] 다트게임

--- programmers/level_2/dartGame.py
+++ programmers/level_2/dartGame.py
@@ -1,30 +1,15 @@
 import re
 
 def solution(dartResult):
-    answer = 0
+    exponent = {'S': 1, 'D': 2, 'T': 3}
+    option = {'': 1, '*': 2, '#': -1}
     p = re.compile("(\d+)([a-zA-Z])(\*|#)?")
     scores = p.findall(dartResult)
-    result = []
-    for idx, score in enumerate(scores):
-        point = int(score[0])
-        exponent = score[1]
-        option = score[2]
-
-        if (exponent == 'S'):
-            exponent = 1
-        elif (exponent == 'D'):
-            exponent = 2
-        elif (exponent == 'T'):
-            exponent = 3
-        if (option == '*'):
-            if (idx == 0):
-                result.append(point**exponent*2)
-            else:
-                result[-1] *= 2
-                result.append(point**exponent*2)
-        elif option == '#':
-            result.append(point**exponent*-1)
-        else: result.append(point**exponent)
-    return sum(result)
+    
+    for i in range(len(scores)):
+        if scores[i][2] == '*' and i > 0:
+            scores[i-2] *= 2
+        scores[i] = int(scores[i][0])**exponent[scores[i][1]]*option[scores[i][2]]
+    return sum(scores)
 
 solution("1S2D*3T")
