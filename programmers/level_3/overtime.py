af849e916d817f6fea670e32a4fc6378cfee50ad
moonelysian <dandyblack@naver.com>
2020-04-01 19:40:55 +0900

    야근 지수

---
+++ programmers/level_3/overtime.py
@@ -0,0 +1,18 @@
+import heapq
+
+def solution(n, works):
+    works = [-work for work in works] 
+    heapq.heapify(works)
+
+    for i in range(n):
+        work = heapq.heappop(works)
+        work += 1
+        if work > 0:
+            work = 0
+        heapq.heappush(works, work)
+
+    works = [work*work for work in works]
+    
+    return sum(works)
+
+solution(4, [4, 3, 3])
